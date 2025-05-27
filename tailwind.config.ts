41d67b65811aac988c7cef083d3cd34d57489459
Jack Liu <feida.liu@medalsoft.com>
2025-05-27 15:59:11 +0800

    feat：setting

---
+++ tailwind.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    // This might be too broad
+    // It will match `packages/**/node_modules` too
+    // '../../packages/**/*.{js,ts,jsx,tsx}',
+  ],
+};
