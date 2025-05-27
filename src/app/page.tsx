2d86bb6328791685802b2167212ee0f1a9f0e36b
Jack Liu <feida.liu@medalsoft.com>
2025-05-27 10:37:45 +0800

    clear：remove MUI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from "@mui/material";
 import Image from "next/image";
 
 export default function Home() {
@@ -17,8 +16,6 @@ export default function Home() {
             Save and see your changes instantly.
           </li>
         </ol>
-        <Button variant="text">Text</Button>
-        <Button variant="contained">Contained</Button>
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
